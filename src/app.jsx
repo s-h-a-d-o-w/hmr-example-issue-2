d17e33f7bebecb2225ca2cab25fe0b5147c24724
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2018-03-28 23:22:47 +0200

    Added branching example to demonstrate that RHL doesn't work as expected.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import Branch1 from './branch1';
+import Branch2 from './branch2';
 
 class App extends Component {
   render() {
     return (<div>
       <h2>Welcome to Lola!</h2>
+      <Branch1/>
+      <Branch2/>
     </div>)
   }
 }
---
+++ src/branch2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        Branch2
+    </div>
+);
